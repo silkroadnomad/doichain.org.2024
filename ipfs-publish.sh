c3aac65abc438865f0df239ab5ed788cd5dd14cc
Nico Krause <post@nicokrause.com>
2024-10-17 13:00:04 +0500

    added git commands and changed cids in vercel.json

--- ipfs-publish.sh
+++ ipfs-publish.sh
@@ -12,4 +12,15 @@ ipfs name publish --key=doichain.org $cid
 # Update the vercel.json file with the new CID
 sed -i '' "s|/ipfs/[^\"}]*|/ipfs/$cid|g" vercel.json
 
-echo 'please commit vercel.json and push it to vercel!'
+# Get the current version from package.json
+version=$(node -p "require('./package.json').version")
+
+# Git commands
+git add vercel.json
+git commit -m "Update IPFS CID to $cid for version $version"
+git tag -a "v$version" -m "Version $version"
+git push origin main
+git push origin --tags
+
+echo "Changes committed and pushed to GitHub. Tagged as v$version"
+echo "Please deploy these changes to Vercel!"
